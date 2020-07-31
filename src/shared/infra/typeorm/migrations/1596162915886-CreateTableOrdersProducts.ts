f3f678aa6df8eb4f3ae92d19397eb3e5728634ad
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-30 23:40:33 -0300

    Criadas tabelas da base de dados faltantes

---
+++ src/shared/infra/typeorm/migrations/1596162915886-CreateTableOrdersProducts.ts
@@ -0,0 +1,73 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTableOrdersProducts1596162915886
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'order_id',
+            type: 'uuid',
+          },
+          {
+            name: 'product_id',
+            type: 'uuid',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 18,
+            scale: 2,
+            default: 0,
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+            default: 0,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'OrdersProductsProduct_FK',
+            referencedTableName: 'products',
+            referencedColumnNames: ['id'],
+            columnNames: ['product_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'OrdersProductsOrder_FK',
+            referencedTableName: 'orders',
+            referencedColumnNames: ['id'],
+            columnNames: ['order_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products');
+  }
+}
